287caefe8a01b1817cc62ce59f0e683e5d19124f
Luccio96 <luka.like.96@gmail.com>
2024-08-08 18:04:45 +0200

    Update my info

--- assets/js/main.js
+++ assets/js/main.js
@@ -211,7 +211,7 @@ themeButton.addEventListener("click", () => {
 
 // Typing Animation using Typed JS
 var typed = new Typed(".type", {
-  strings: ["a Web", "an Android", "a Blockchain"],
+  strings: ["un software engineer", "un web delevoper", "IT administrator"],
   smartBackspace: true,
   startDelay: 1000,
   typeSpeed: 130,
